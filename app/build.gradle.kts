cc811cb310346a23bc3a2c5cb0a3731e8d23c644
ThomahPerinpanayagam <tkp7787@rit.edu>
2023-10-18 20:33:25 -0400

    Loading screen added, user interface for data storage added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.zybooks.loginapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.zybooks.loginapp"
@@ -39,4 +39,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("com.google.android.material:material:1.10.0")
 }
---
+++ app/src/main/java/com/zybooks/loginapp/StoreData.java
@@ -0,0 +1,13 @@
+package com.zybooks.loginapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StoreData extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_store_data);
+    }
+}
